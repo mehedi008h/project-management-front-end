bbb119b09d8d73cabf20d4aa878b057a1420e6bf
Mehedi Hasan <mehedi08h@gmail.com>
2023-07-25 23:41:11 +0600

    project setup with chakra ui

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Text } from "@chakra-ui/react";
+
+function App() {
+    return (
+        <>
+            <Text textAlign="center">Hello World</Text>
+        </>
+    );
+}
+
+export default App;
